ca1d16cf2275ddb445aff208ccbc1397dfd92eeb
birdmaaan <eb.d@outlook.com>
2020-08-21 14:19:20 -0700

    remove null constraint from registered user

---
+++ db/migrate/20200821211804_remove_null_constraint.rb
@@ -0,0 +1,5 @@
+class RemoveNullConstraint < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :users, :registered, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_195051) do
+ActiveRecord::Schema.define(version: 2020_08_21_211804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_195051) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "registered", default: true, null: false
+    t.boolean "registered", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
