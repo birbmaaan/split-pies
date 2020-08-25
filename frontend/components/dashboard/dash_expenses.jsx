b1cf4e755a979033997543aca644c11b2d4548e0
birdmaaan <eb.d@outlook.com>
2020-08-24 22:51:51 -0700

    add no bill image, touch up bill css

--- frontend/components/dashboard/dash_expenses.jsx
+++ frontend/components/dashboard/dash_expenses.jsx
@@ -5,7 +5,7 @@ class DashExpenses extends React.Component {
   render() {
     return (
       <div className="dash-expenses">
-        <img src="" alt="No expense guy"/>
+        <img src={window.nobill} alt="all paid up!" /> 
         <div>
           <h1>You have not added any expenses yet</h1>
           <p>To add a new expense, click the orange "Add an expense" button.</p>
