d0efcec483e2e7f48109f4328bfca39932e19ea3
birdmaaan <eb.d@outlook.com>
2020-09-17 11:12:54 -0700

    add splash links

--- frontend/components/splash_page/splash.jsx
+++ frontend/components/splash_page/splash.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 class Splash extends React.Component {
 
@@ -12,6 +14,14 @@ class Splash extends React.Component {
             <h1>Less stress when sharing expenses</h1>
             <p>Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.</p>
             <Link to='/signup'><button>Sign up</button></Link>
+            <div className="about-links-splash">
+              <a href="https://github.com/birbmaaan/">
+                <FontAwesomeIcon className="icon-links" icon={faGithubSquare} />
+              </a>
+              <a href="https://www.linkedin.com/in/elijah-dove-4770b3184/">
+                <FontAwesomeIcon className="icon-links" icon={faLinkedin} />
+              </a>
+            </div>
           </div>
           <img src={window.pie} alt="have your pie and eat it too" />
         </section>
